04f52a1d4ad08c7714781044f4dff68a3de6dd76
Serge <sergiuhowie@campus.masterschool.com>
2023-08-12 01:43:40 +0100

    "New personal development"

--- app.py
+++ app.py
@@ -2,15 +2,30 @@ import sqlalchemy
 import jinja2
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy import create_engine, Table, Column, Integer, String, text, ForeignKey
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import MetaData
 
+engine = create_engine("sqlite:///library.sqlite", echo=True)
 
-engine = create_engine('sqlite:///library.sqlite', echo=True)
+with engine.connect() as conn:
+    metadata_obj = MetaData()
 
-stmt = text("SELECT x, y FROM Authors WHERE y > :y ORDER BY x, y")
-with Session(engine) as session:
-    result = session.execute(stmt, {"y": 6})
-    for row in result:
-        print(f"x")
+    user_table = Table(
+        "user_account",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(30)),
+        Column("fullname", String),
+    )
+
+    address_table = Table(
+        "address",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("user_id", ForeignKey("user_account.id"), nullable=False),
+        Column("email_address", String, nullable=False),
+    )
+
+    metadata_obj.create_all(engine)
